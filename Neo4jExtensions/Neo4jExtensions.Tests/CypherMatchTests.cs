21ba92f556933f446d342f296044d79f1416395f
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-06-06 09:37:06 +0300

    testing syntax

--- Neo4jExtensions/Neo4jExtensions.Tests/CypherMatchTests.cs
+++ Neo4jExtensions/Neo4jExtensions.Tests/CypherMatchTests.cs
@@ -17,13 +17,18 @@ namespace Neo4jExtensions.Tests
 
             ICypherFluentQuery query = null;
 
-            query = query.MatchBuilder<Tariff>()
+            Func<IRelationCypherBuilder<Route>, string> rel2RouteBuilder = x => x.Where(r => r.Order == 1).Build();
+
+            Func<IMatchCypherBuilder<Location>, string> matchLocBuilder = m => m.Where(l => l.Id == locationId)
+                .Rel(rel2RouteBuilder).Build();
+            Func<IRelationCypherBuilder<Route>, string> relRouteBuilder = r => 
+                r.Where(route => route.Order == 1).Where(route => route.Condition == RouteCondition.FOB)
+                .To(matchLocBuilder).Build();
+
+            var match = query.MatchBuilder<Tariff>()
                 .Where(t => t.Number == "number").Where(t => t.Id == tariffId)
                 .Where(t => t.Type == Kind.Auto).Where(t => t.Version == 1)
-                .Rel<Route>()
-                .Where(r => r.Order == 1).Where(r => r.Condition == RouteCondition.FOB)
-                .To<Location>().Where(l => l.Id == locationId)
-                .Build();
+                .Rel(relRouteBuilder);
         }
     }
 }
--- Neo4jExtensions/Neo4jExtensions/IMatchCypherBuilder.cs
+++ Neo4jExtensions/Neo4jExtensions/IMatchCypherBuilder.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using System.Linq.Expressions;
-using Neo4jClient.Cypher;
 
 namespace Neo4jExtensions
 {
     public interface IMatchCypherBuilder<T>
     {
         IMatchCypherBuilder<T> Match(string target = "x");
-        IRelationCypherBuilder<TRel> Rel<TRel>(string rel = "r");
+        IRelationCypherBuilder<TRel> Rel<TRel>(Func<IRelationCypherBuilder<TRel>, string> relBuilder, string rel = "r");
         IMatchCypherBuilder<T> Where(Expression<Func<T, bool>> predicate);
 
-        ICypherFluentQuery Build();
+        string Build();
     }
 
     public interface IRelationCypherBuilder<TRel>
     {
-        IMatchCypherBuilder<T> To<T>(string target = "xt");
-        IMatchCypherBuilder<T> From<T>(string target = "xt");
+        IMatchCypherBuilder<T> To<T>(Func<IMatchCypherBuilder<T>, string> matchBuilder, string target = "xt");
+        IMatchCypherBuilder<T> From<T>(Func<IMatchCypherBuilder<T>, string> matchBuilder, string target = "xt");
         IRelationCypherBuilder<TRel> Where(Expression<Func<TRel, bool>> predicate);
+
+        string Build();
     }
 }
